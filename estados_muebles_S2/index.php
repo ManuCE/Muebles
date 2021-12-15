c7decd736f7f3a7c31860ec38747670da927db1a
Mario <43281444+ManuCE@users.noreply.github.com>
2021-12-14 21:37:57 -0300

    Add files via upload

---
+++ estados_muebles_S2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+
+
+
+<nav class="navbar navbar-dark bg-dark">
+  <!-- Navbar content -->
+  <a class="btn btn-primary" href="tabla_muebles.php" role="button">Tabla Muebles</a>
+  <a class="btn btn-primary" href="tabla_estado.php" role="button">Tabla Estado</a>
+  <a class="btn btn-primary" href="tabla_usuario.php" role="button">Tabla Usuario</a>
+  <a class="btn btn-primary" href="tabla_control.php" role="button">Tabla Control</a>
+
+
+
+</nav>
+
+</body>
+</html>
